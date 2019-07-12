e1958fd39b3cef6ceea28b6bf333f03a3acb924b
Dereck Bridie <me@devbridie.com>
2019-07-12 18:03:18 +0200

    Fix method to run the transformer

--- src/main/kotlin/com/devbridie/jbinterview/MainCommand.kt
+++ src/main/kotlin/com/devbridie/jbinterview/MainCommand.kt
@@ -4,15 +4,19 @@ import com.devbridie.jbinterview.destructure.transformDestructure
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.file
+import java.lang.RuntimeException
 
-class Main : CliktCommand() {
+class MainCommand : CliktCommand() {
     val input by argument("Input file").file(exists = true, folderOkay = false, readable = true)
     override fun run() {
-        val ast = parseFile(input)
+        val ast = kotlin.runCatching { parseFile(input) }.getOrElse {
+            throw RuntimeException("Unable to parse JavaScript file at ${input.absolutePath}", it)
+        }
+
         val transformed = transformDestructure(ast)
         val result = transformed.toSource()
         echo(result)
     }
 }
 
-fun main(args: Array<String>) = Main().main(args)
+fun main(args: Array<String>) = MainCommand().main(args)
